25b586a166173c3e46a5880ccb78b087b3865fde
Micael Morais <micaelmor@gmail.com>
2020-05-10 13:42:38 +0100

    Initial commit

---
+++ ReleaseErrorTest/DomainTypes.fs
@@ -0,0 +1,25 @@
+﻿namespace ReleaseErrorTest
+
+module DbConnection =
+
+    open System.Data
+    open FSharp.Data.Sql
+    open FSharp.Core
+
+
+    let [<Literal>] dbVendor = Common.DatabaseProviderTypes.POSTGRESQL
+
+    let [<Literal>] connString = "Host=34.105.253.30;Port=5432;Database=postgres;Username=postgres;Password=vrt5LppzCi4dDMKB"
+    
+    let [<Literal>] resPath = __SOURCE_DIRECTORY__  + @"/../npgsql/"
+
+    let [<Literal>] indivAmount = 1000
+
+    let [<Literal>] useOptTypes  = true
+
+    let [<Literal>] owner = "public"
+
+    type DbMdb =
+        SqlDataProvider<dbVendor, connString,"",resPath,indivAmount,useOptTypes,owner>
+
+    let getDbContext() = DbMdb.GetDataContext()
---
+++ ReleaseErrorTest/ReleaseErrorTest.fs
@@ -0,0 +1,47 @@
+﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
+namespace ReleaseErrorTest
+
+open System.Diagnostics
+open Fabulous
+open Fabulous.XamarinForms
+open Fabulous.XamarinForms.LiveUpdate
+open Xamarin.Forms
+
+module App = 
+    type Model = 
+      { ErrorMessage: string }
+
+    type Msg = 
+        | GetContext 
+
+    let initModel = { ErrorMessage = "Press Button" }
+
+    let init () = initModel, Cmd.none
+
+    let update msg model =
+        match msg with
+        | GetContext ->
+            try
+                let ctx = DbConnection.getDbContext()
+                {model with ErrorMessage = "No error, it worked"} , Cmd.none
+            with
+            | e -> {model with ErrorMessage = e.Message}, Cmd.none
+
+    let view (model: Model) dispatch =
+        View.ContentPage(
+          content = View.StackLayout(padding = Thickness 20.0, verticalOptions = LayoutOptions.Center,
+            children = [ 
+                View.Label(text = sprintf "%s" model.ErrorMessage, horizontalOptions = LayoutOptions.Center, width=300.0, horizontalTextAlignment=TextAlignment.Center)
+                View.Button(text = "GetContext", command = (fun () -> dispatch GetContext), horizontalOptions = LayoutOptions.Center)
+            ]))
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program = XamarinFormsProgram.mkProgram init update view
+
+type App () as app = 
+    inherit Application ()
+
+    let runner = 
+        App.program
+        |> Program.withConsoleTrace
+        |> XamarinFormsProgram.run app
